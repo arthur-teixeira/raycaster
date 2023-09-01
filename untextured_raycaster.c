2df009611351d830365136fae3991f85b972134b
arthur-carvalho <arthur.teixeira@objective.com.br>
2023-09-01 20:45:25 -0300

    initial raycaster implementation (not working correctly)

--- untextured_raycaster.c
+++ untextured_raycaster.c
@@ -1,6 +1,7 @@
-#include <raylib.h>
-#include <stdio.h>
 #include <math.h>
+#include <raylib.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 #define mapWidth 24
 #define mapHeight 24
@@ -34,7 +35,33 @@ int worldMap[mapWidth][mapHeight] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 };
 
-int main(int argc, char **argv) {
+void calculateStepAndSideDist(int rayDir, int pos, int map, int deltaDist,
+                              int *step, double *sideDist) {
+  if (rayDir < 0) {
+    *step = 1;
+    *sideDist = (pos - map) * deltaDist;
+  } else {
+    *step = 1;
+    *sideDist = (map + 1.0f - pos) * deltaDist;
+  }
+}
+
+Color getColor(int location) {
+  switch (location) {
+  case 1:
+    return RED;
+  case 2:
+    return GREEN;
+  case 3:
+    return BLUE;
+  case 4:
+    return WHITE;
+  default:
+    return YELLOW;
+  }
+}
+
+int main(void) {
   double posX = 22, posY = 12;
   double dirX = -1, dirY = 0;
   double planeX = 0, planeY = 0.66f;
@@ -47,7 +74,8 @@ int main(int argc, char **argv) {
     BeginDrawing();
 
     for (int x = 0; x < screenWidth; x++) {
-      double cameraX = 2 * x / (double)screenWidth - 1; // The x coordinate in camera space
+      double cameraX =
+          2 * x / (double)screenWidth - 1; // The x coordinate in camera space
       double rayDirX = dirX + planeX * cameraX;
       double rayDirY = dirY + planeY * cameraX;
 
@@ -60,12 +88,103 @@ int main(int argc, char **argv) {
       double sideDistY;
 
       // Distance from one x-y side to the next x-y side
-      double deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
-      double deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
+      double deltaDistX = (rayDirX == 0) ? INFINITY : fabs(1 / rayDirX);
+      double deltaDistY = (rayDirY == 0) ? INFINITY : fabs(1 / rayDirY);
 
+      // What direction to step in each axis
       int stepX;
       int stepY;
+
+      calculateStepAndSideDist(rayDirX, posX, currentMapSquareX, deltaDistX,
+                               &stepX, &sideDistX);
+      calculateStepAndSideDist(rayDirY, posY, currentMapSquareY, deltaDistY,
+                               &stepY, &sideDistY);
+
+      bool hit = false;
+      int side; // North-South or East-West
+
+      // Perform DDA
+      while (!hit) {
+        // Jump to next square, either in the x or y direction
+        if (sideDistX < sideDistY) {
+          sideDistX += deltaDistX;
+          currentMapSquareX += stepX;
+          side = 0;
+        } else {
+          sideDistY += deltaDistY;
+          currentMapSquareY += stepY;
+        }
+
+        hit = (worldMap[currentMapSquareX][currentMapSquareY] > 0);
+      }
+
+      double perpendicularWallDistance;
+      if (side == 0) { // Hit on x axis
+        perpendicularWallDistance = (sideDistX - deltaDistX);
+      } else {
+        perpendicularWallDistance = (sideDistY - deltaDistY);
+      }
+
+      // Height of line to draw on the screen
+      int lineHeight = (int)(screenHeight / perpendicularWallDistance);
+
+      int drawStart = (-lineHeight + screenHeight) / 2;
+      if (drawStart < 0)
+        drawStart = 0;
+
+      int drawEnd = (lineHeight + screenHeight) / 2;
+      if (drawEnd < 0)
+        drawEnd = 0;
+
+      Color color = getColor(worldMap[currentMapSquareX][currentMapSquareY]);
+      if (side == 1) {
+        color.a /= 2; // If the wall was hit sideways, show it darker
+      }
+
+      DrawLine(x, drawStart, x, drawEnd, color);
     }
+
+    oldTime = time;
+    time = GetTime();
+    double frameTime = time - oldTime; // GetFrameTime()?
+
+    double moveSpeed = frameTime * 5.0f;
+    double rotSpeed = frameTime * 3.0f;
+    if (IsKeyPressed(KEY_UP)) {
+        if (worldMap[(int)(posX + dirX * moveSpeed)][(int)posY] == 0) {
+            posX += dirX *moveSpeed;
+        }
+        if (worldMap[(int)posX][(int)(posY + dirY * moveSpeed)] == 0) {
+            posY += dirY *moveSpeed;
+        }
+    }
+    if (IsKeyPressed(KEY_DOWN)) {
+        if (worldMap[(int)(posX - dirX * moveSpeed)][(int)posY] == 0) {
+            posX += dirX *moveSpeed;
+        }
+        if (worldMap[(int)posX][(int)(posY - dirY * moveSpeed)] == 0) {
+            posY += dirY *moveSpeed;
+        }
+    }
+    if (IsKeyPressed(KEY_LEFT)) {
+        double oldDirX = dirX;
+        dirX = dirX * cos(-rotSpeed) - dirY * sin(-rotSpeed);
+        dirY = oldDirX * sin(-rotSpeed) + dirY * cos(-rotSpeed);
+
+        double oldPlaneX = planeX;
+        planeX = planeX * cos(-rotSpeed) - planeY * sin(-rotSpeed);
+        planeY = oldPlaneX * sin(-rotSpeed) + planeY * cos(-rotSpeed);
+    }
+    if (IsKeyPressed(KEY_RIGHT)) {
+        double oldDirX = dirX;
+        dirX = dirX * cos(rotSpeed) - dirY * sin(rotSpeed);
+        dirY = oldDirX * sin(rotSpeed) + dirY * cos(rotSpeed);
+
+        double oldPlaneX = planeX;
+        planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
+        planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
+    }
+
 
     EndDrawing();
   }
