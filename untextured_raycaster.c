ec090a9098e45e5a58183623f7db42fcc722ea75
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2023-09-02 14:35:34 -0300

    Fixed sideways hit rendering

--- untextured_raycaster.c
+++ untextured_raycaster.c
@@ -35,17 +35,6 @@ int worldMap[mapWidth][mapHeight] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 };
 
-void calculateStepAndSideDist(int rayDir, int pos, int map, int deltaDist,
-                              int *step, double *sideDist) {
-  if (rayDir < 0) {
-    *step = 1;
-    *sideDist = (pos - map) * deltaDist;
-  } else {
-    *step = 1;
-    *sideDist = (map + 1.0f - pos) * deltaDist;
-  }
-}
-
 Color getColor(int location) {
   switch (location) {
   case 1:
@@ -79,8 +68,8 @@ int main(void) {
       double rayDirY = dirY + planeY * cameraX;
 
       // Which box of the map we are currently in.
-      int currentMapSquareX = (int)posX;
-      int currentMapSquareY = (int)posY;
+      int mapX = (int)posX;
+      int mapY = (int)posY;
 
       // Length of the ray from the current position to the next x-y side
       double sideDistX;
@@ -94,10 +83,21 @@ int main(void) {
       int stepX;
       int stepY;
 
-      calculateStepAndSideDist(rayDirX, posX, currentMapSquareX, deltaDistX,
-                               &stepX, &sideDistX);
-      calculateStepAndSideDist(rayDirY, posY, currentMapSquareY, deltaDistY,
-                               &stepY, &sideDistY);
+      if (rayDirX < 0) {
+        stepX = -1;
+        sideDistX = (posX - mapX) * deltaDistX;
+      } else {
+        stepX = 1;
+        sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+      }
+
+      if (rayDirY < 0) {
+        stepY = -1;
+        sideDistY = (posY - mapY) * deltaDistY;
+      } else {
+        stepY = 1;
+        sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+      }
 
       bool hit = false;
       int side; // North-South or East-West
@@ -107,20 +107,21 @@ int main(void) {
         // Jump to next square, either in the x or y direction
         if (sideDistX < sideDistY) {
           sideDistX += deltaDistX;
-          currentMapSquareX += stepX;
+          mapX += stepX;
           side = 0;
         } else {
           sideDistY += deltaDistY;
-          currentMapSquareY += stepY;
+          mapY += stepY;
+          side = 1;
         }
 
-        hit = (worldMap[currentMapSquareX][currentMapSquareY] > 0);
+        hit = (worldMap[mapX][mapY] > 0);
       }
 
       double perpendicularWallDistance;
       if (side == 0) { // Hit on x axis
         perpendicularWallDistance = (sideDistX - deltaDistX);
-      } else {
+      } else { // Hit on Y axis
         perpendicularWallDistance = (sideDistY - deltaDistY);
       }
 
@@ -135,7 +136,7 @@ int main(void) {
       if (drawEnd < 0)
         drawEnd = 0;
 
-      Color color = getColor(worldMap[currentMapSquareX][currentMapSquareY]);
+      Color color = getColor(worldMap[mapX][mapY]);
       if (side == 1) {
         color.a /= 2; // If the wall was hit sideways, show it darker
       }
@@ -145,8 +146,8 @@ int main(void) {
 
     double frameTime = GetFrameTime();
 
-    double moveSpeed = frameTime * 2.0f;
-    double rotSpeed = frameTime;
+    double moveSpeed = frameTime;
+    double rotSpeed = frameTime *0.75f;
 
     ClearBackground(BLACK);
     EndDrawing();
@@ -161,10 +162,10 @@ int main(void) {
     }
     if (IsKeyDown(KEY_DOWN)) {
       if (worldMap[(int)(posX - dirX * moveSpeed)][(int)posY] == 0) {
-        posX += dirX * moveSpeed;
+        posX -= dirX * moveSpeed;
       }
       if (worldMap[(int)posX][(int)(posY - dirY * moveSpeed)] == 0) {
-        posY += dirY * moveSpeed;
+        posY -= dirY * moveSpeed;
       }
     }
     if (IsKeyDown(KEY_RIGHT)) {
